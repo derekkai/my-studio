65952f2b2b6753e872f3283e5bc4021545996b81
DerekLIn <derek@yaudian.com>
2021-02-26 14:02:16 +0800

    feat: basic implements.

---
+++ pages/post.js
@@ -0,0 +1,48 @@
+import sanityClient from "./client";
+import Link from 'next/link'
+
+const post = ({data}) => {
+    console.log(data);
+    return <div>
+        This is my post!
+        <Link href="/home">test</Link>
+        <div>
+            {
+                data.map(post =>
+                    <Link href={`post/${post.slug.current}`} key={post.slug.current}>
+                        <a>
+                            <h3>
+                                {post.title}
+                            </h3>
+                            <img src={post.mainImage.asset.url} alt={post.mainImage.alt}/>
+                        </a>
+                    </Link>,
+                )
+            }
+        </div>
+    </div>
+}
+
+export const getServerSideProps = async () => {
+
+    const query = `*[_type == "post"]{
+        title,
+        slug,
+        mainImage{
+            asset->{
+                _id,
+                url
+            },
+            alt
+        }
+    }`;
+    const result = await sanityClient.fetch(query)
+
+    return {
+        props: {
+            data: result,
+        },
+    }
+}
+
+export default post;
--- pages/post/[slug].js
+++ pages/post/[slug].js
@@ -1,5 +1,44 @@
-const Post = () => {
-    return <div>This is my first post!</div>
+import {useState, useEffect} from 'react';
+import sanityClient from "../client";
+
+
+const Post = ({ title, image}) => {
+    console.log(title, image);
+    return <div>
+        This is my first post!
+        <img src={image.asset.url}/>
+    </div>
 }
 
+export const getServerSideProps = async pageContext => {
+    const pageSlug = pageContext.query.slug;
+
+    if (!pageSlug) {
+        return {
+            notFound: true,
+        }
+    }
+
+    const query = `*[_type == "post" && slug.current == "${pageSlug}"]{
+        title,
+        slug,
+        mainImage{
+            asset->{
+                _id,
+                url
+            },
+            alt
+        }
+    }`;
+    console.log(query)
+    const result = await sanityClient.fetch(query).then(res => res[0])
+    console.log(result);
+
+    return {
+        props: {
+            title: result.title,
+            image: result.mainImage,
+        },
+    }
+}
 export default Post;
